5f0694633c1f2961096bdda2159c2ff660eb531b
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-11-18 01:21:23 -0800

    Added the pretty package.

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"learning/textbasics"
+
+	"github.com/kr/pretty"
+)
+
+func main() {
+	filename := "/Users/shadon./Developer/golang_stuff/learning/textbasics/info_text.txt"
+	info, err := textbasics.ParseFileWithRegex(filename)
+	if err != nil {
+		panic(err.Error())
+	}
+	pretty.Print(info)
+}
--- textbasics/using_regex.go
+++ textbasics/using_regex.go
@@ -1,7 +1,9 @@
 package textbasics
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"regexp"
 	"strconv"
 )
@@ -29,5 +31,33 @@ func ParseRegex(line string) (Information, error) {
 	result.name = matches[1]
 	result.age, _ = strconv.Atoi(matches[2])
 	result.location = matches[3]
+	return result, nil
+}
+
+func ParseFileWithRegex(filename string) ([]Information, error) {
+	var result []Information
+	file, err := os.Open(filename)
+	if err != nil {
+		return []Information{}, fmt.Errorf("unable to open the file %q", filename)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			// get out once we get to the end of the line
+			// fmt.Printf("End of line reached at line: %q\n", line)
+			break
+		}
+		// now parse the line using the function defined above
+		info, err := ParseRegex(line)
+		if err != nil {
+			return []Information{}, err
+		}
+		// now we got the info, append it to result
+		result = append(result, info)
+	}
+
 	return result, nil
 }
