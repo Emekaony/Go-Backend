f6b005bed7a8ef4f9d83bcbe8a68d57c4c773984
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-11-30 21:38:33 -0800

    Managed to set the response header so it knows we're serving json and not plain text

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"banking/models"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	// http routes
 	http.HandleFunc("/greet", greet)
-
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	fmt.Println("Server is listening on port 8080")
-	http.ListenAndServe("localhost:8000", nil)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +24,14 @@ func greet(w http.ResponseWriter, r *http.Request) {
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	// make a list of customers to send to the client
+	w.Header().Set("Content-Type", "application/json")
+	customers := []models.Customer{
+		{Name: "nnaemeka", City: "Sacramento", Zipcode: "223"},
+		{Name: "kamsi", City: "Coulumbus", Zipcode: "8900"},
+	}
+	json.NewEncoder(w).Encode(customers)
 }
---
+++ models/customer.go
@@ -0,0 +1,7 @@
+package models
+
+type Customer struct {
+	Name    string
+	City    string
+	Zipcode string
+}
